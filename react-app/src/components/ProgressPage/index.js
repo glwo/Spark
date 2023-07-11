db8360c114c6d2de8b3e2ebf2d343e012e14eec1
glen <glenwojnar@yahoo.com>
2023-07-11 15:35:18 -0400

    progress page function and styling

--- react-app/src/components/ProgressPage/index.js
+++ react-app/src/components/ProgressPage/index.js
@@ -4,12 +4,13 @@ import { fetchProgress } from '../../store/progress';
 import { Line } from 'react-chartjs-2';
 import Chart from 'chart.js';
 import moment from 'moment';
-
+import "./ProgressPage.css";
 
 const ProgressGraph = () => {
   const user = useSelector((state) => state.session.user);
   const allProgressData = useSelector((state) => state.progress.progressList.progress_entries);
   const dispatch = useDispatch();
+  const [selectedVariable, setSelectedVariable] = useState('weight'); // Default selected variable is 'weight'
 
   useEffect(() => {
     dispatch(fetchProgress());
@@ -27,20 +28,27 @@ const ProgressGraph = () => {
     return <p>No progress data available.</p>; // Display a message if no progress data is found for the user
   }
 
+  const getVariableLabel = (variable) => {
+    switch (variable) {
+      case 'weight':
+        return 'Weight (lbs)';
+      case 'body_fat_percentage':
+        return 'Body Fat (%)';
+      case 'metabolic_age':
+        return 'Metabolic Age (years)';
+      default:
+        return '';
+    }
+  };
+
   const chartData = {
     labels: progressData.map((entry) => moment(entry.progress_date).toDate()),
     datasets: [
       {
-        label: 'Weight',
-        data: progressData.map((entry) => entry.weight),
+        label: getVariableLabel(selectedVariable), // Use the custom label for the selected variable
+        data: progressData.map((entry) => entry[selectedVariable]), // Access the selected variable dynamically
         fill: false,
-        borderColor: '#8884d8',
-      },
-      {
-        label: 'Body Fat Percentage',
-        data: progressData.map((entry) => entry.body_fat_percentage),
-        fill: false,
-        borderColor: '#82ca9d',
+        borderColor: '#efaf00',
       },
     ],
   };
@@ -52,7 +60,7 @@ const ProgressGraph = () => {
         time: {
           tooltipFormat: 'll',
           displayFormats: {
-            day: 'MMM D', // Use the desired format for displaying the date
+            day: 'MMM D',
           },
         },
         ticks: {
@@ -62,10 +70,33 @@ const ProgressGraph = () => {
     },
   };
 
+  const handleVariableChange = (variable) => {
+    setSelectedVariable(variable);
+  };
 
   return (
-    <div>
-      <h2>Progress Graph</h2>
+    <div className="graph-container">
+      <h2>Your Progress</h2>
+      <div className="variable-selector">
+        <button
+          className={selectedVariable === 'weight' ? 'active' : ''}
+          onClick={() => handleVariableChange('weight')}
+        >
+          {getVariableLabel('weight')}
+        </button>
+        <button
+          className={selectedVariable === 'body_fat_percentage' ? 'active' : ''}
+          onClick={() => handleVariableChange('body_fat_percentage')}
+        >
+          {getVariableLabel('body_fat_percentage')}
+        </button>
+        <button
+          className={selectedVariable === 'metabolic_age' ? 'active' : ''}
+          onClick={() => handleVariableChange('metabolic_age')}
+        >
+          {getVariableLabel('metabolic_age')}
+        </button>
+      </div>
       {progressData.length > 0 ? (
         <Line data={chartData} options={chartOptions} />
       ) : (
